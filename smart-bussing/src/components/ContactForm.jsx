ea79d363ab717d5ceeaa0977738476e6388572ac
Pepino8 <cristobalcoronador@gmail.com>
2026-05-08 17:33:59 -0700

    tuneo

--- smart-bussing/src/components/ContactForm.jsx
+++ smart-bussing/src/components/ContactForm.jsx
@@ -82,7 +82,7 @@ const ContactForm = () => {
       const finalData = { ...formData, urlFiles };
 
       const response = await fetch(
-        "https://smart-bussing-back.onrender.com/api/v1/registrarLugar",
+        "https://smart-bussing-back.onrender.com/api/v1/registrarLuga",
         {
           method: "POST",
           headers: {
