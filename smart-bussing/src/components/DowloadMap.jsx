497da872b0599e73e54c1bb18bfa5e0a5bc18a1a
raulvaldes-cetys <raul.valdes@cetys.edu.mx>
2026-05-12 00:37:27 -0700

    feat: Add loading indicator to DowloadMap component for improved user experience

--- smart-bussing/src/components/DowloadMap.jsx
+++ smart-bussing/src/components/DowloadMap.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
 import HomeDecor from "../assets/home-decor.svg"
 
 const DowloadMap = () => {
+    const [loaded, setLoaded] = useState(false);
+
     return (
-        <div className="w-full flex justify-center">
-            <img src={HomeDecor} alt="Home decoration" style={{ width: "484px", height: "507px" }} className="z-10"/>
+        <div className="w-full flex justify-center" style={{ width: "484px", height: "507px" }}>
+            {!loaded && (
+                <div className="flex items-center justify-center w-full h-full">
+                    <CircularProgress sx={{ color: "white" }} />
+                </div>
+            )}
+            <img
+                src={HomeDecor}
+                alt="Home decoration"
+                style={{ width: "484px", height: "507px", display: loaded ? "block" : "none" }}
+                className="z-10"
+                onLoad={() => setLoaded(true)}
+            />
         </div>
     )
 }
