4ffa71a6ed9097aae560a4f55e3b82f6c8fe3088
Pepino8 <cristobalcoronador@gmail.com>
2026-05-11 20:34:20 -0700

    Changed year to 2026

--- smart-bussing/src/components/Footer.jsx
+++ smart-bussing/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <div className="bg-[#fcfcf1] items-center justify-center text-center">
             <div className="text-[#3B7C5F] py-5">
-                © Smart Bussing - 2025
+                © Smart Bussing - 2026
             </div>
         </div>
     )
